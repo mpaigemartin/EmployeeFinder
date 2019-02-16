541822e2b8373c61689f24a87e35b028c66585a2
Paige <mpaigemartin@gmail.com>
2019-02-15 22:50:11 -0500

    javascript rework

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,45 @@
 const employees = require("../data/employees");
 
-
+module.exports = function(app) {
+    
 app.get("/api/employees", function(req, res){
-    res.json(employeeList)
+    res.json(employees)
 });
 
 app.post("/api/employees", function(req, res){
-    res.json(req.body)
-});
+    const strUser = req.body;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    const sum = function(strUser){
+        let scores = strUser.scores
+        let reducedScores = scores.map(Number)
+        let userSum = reducedScores.reduce(reducer)
+        return userSum
+    }
+
+    let userSum = sum(strUser)
+    let lowestDifference = 100;
+    let employeeRank = 100;
+    let employeeSelection;
+    let employeeSum = 0;
+
+    for (let i = 0; i < employeeList.length; i++) {
+        employeeSum = 0; 
+        employeeSum = employeesList[i].scores.reduce(reducer);
+        employeeRank =  Math.abs(parseInt(userSum) - parseInt(employeeSum));
+
+        if (employeeRank<=lowestDifference){
+            lowestDifference=employeeRank;
+            employeeSelection=i;
+        }
+    }
+    const selection = {
+        "name": employeesList[i].name,
+        "photo": employeesList[i].link,
+        "rank": employeeRank
+    };
+
+    employeeList.push(strUser);
+    res.json(selection);
+
+})};
--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const server = require("../data/routing/apiRoutes")
 
 const app = express();
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
 
 app.listen(PORT, function(){
     console.log(`Server is listening on: http://localhost:${PORT}`);
