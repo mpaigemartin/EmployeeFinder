53ae26865bb09425b2632226e64134fea37cfd27
Paige <mpaigemartin@gmail.com>
2019-02-20 09:41:16 -0500

    adds some fixes to server and package.json

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
-const server = require("../data/routing/apiRoutes")
-
 const app = express();
-
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'app/public')));
 
