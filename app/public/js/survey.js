4518197369088853f78d736bf2f20b2397e06e88
Paige <mpaigemartin@gmail.com>
2019-03-29 17:53:55 -0400

    i think this is now C worthy

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,9 +1,10 @@
-const validation = function() {
+$(function() {
+  // const validation = function() {
   const validateForm = function() {
     let isValid = true;
 
     $("input").each(function() {
-      if (!$this.val()) {
+      if (!$(this).val()) {
         isValid = false;
       }
     });
@@ -16,87 +17,91 @@ const validation = function() {
 
     return isValid;
   };
-};
 
-const myModal = function(data) {
-  $("#match-name").text(data.name);
-  $("#match-img").attr("src", data.photo);
+  const displayModal = function(data) {
+    console.log("modal");
+    $("#match-name").text(data.name);
+    $("#match-img").attr("src", data.photo);
 
-  $("#results-modal").modal("toggle");
-};
-
-const answers = [];
-
-const e = document.getElementById("select");
-const strUser = e.options[e.selectedIndex].value;
-
-const results = function() {
-  answers.push(document.getElementById("strUser").value);
-  employeeSelection.push(`${newEmployee}`);
-};
-
-const submit = function(e) {
-  e.preventDefault();
-  if (validateForm()) {
-    const strUser = {
-      name: $("#name").val(),
-      photo: $("#link").val(),
-      scores: [
-        $("question1").val(),
-        $("question2").val(),
-        $("question3").val(),
-        $("question4").val(),
-        $("question5").val(),
-        $("question6").val(),
-        $("question7").val(),
-        $("question8").val(),
-        $("question9").val(),
-        $("question10").val()
-      ]
-    };
-    $.post("/api/employees", strUser, myModal);
-  } else {
-    $("#error").text("All fields are required");
-  }
-  $("#submit").on("click", submit);
-};
-
-$("#myModal").on("shown.bs.modal", function() {
-  $("#myInput").trigger("focus");
-});
-
-const employeeSelection = function(selection, submit) {};
-
-const employeeEquation = function() {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-  const sum = function(strUser) {
-    let scores = strUser.scores;
-    let reducedScores = scores.map(Number);
-    let userSum = reducedScores.reduce(reducer);
-    return userSum;
-
-    let userSum = sum(strUser);
-    let lowestDifference = 100;
-    let employeeRank = 100;
-    let employeeSelection;
-    let employeeSum = 0;
-
-    for (let i = 0; i < employeeList.length; i++) {
-      employeeSum = employeesList[i].scores.reduce(reducer);
-      employeeRank = Math.abs(parseInt(userSum) - parseInt(employeeSum));
-
-      if (employeeRank <= lowestDifference) {
-        lowestDifference = employeeRank;
-        employeeSelection = i;
-      }
-    }
-    const selection = {
-      name: employeesList[i].name,
-      photo: employeesList[i].link,
-      rank: employeeRank
-    };
+    $("#results-modal").modal("toggle");
   };
-};
 
-$("#submit").on("click", submit);
+  const answers = [];
+
+  // const e = document.getElementById("select");
+  // const strUser = e.options[e.selectedIndex].value;
+
+  const results = function() {
+    answers.push(document.getElementById("strUser").value);
+    employeeSelection.push(`${newEmployee}`);
+  };
+
+  const submit = function(event) {
+    event.preventDefault();
+    console.log("submit");
+    if (validateForm()) {
+      console.log("valid");
+      const strUser = {
+        name: $("#name").val(),
+        photo: $("#link").val(),
+        scores: [
+          $("#question1").val(),
+          $("#question2").val(),
+          $("#question3").val(),
+          $("#question4").val(),
+          $("#question5").val(),
+          $("#question6").val(),
+          $("#question7").val(),
+          $("#question8").val(),
+          $("#question9").val(),
+          $("#question10").val()
+        ]
+      };
+      $.post("/api/employees", strUser, displayModal);
+    } else {
+      $("#error").text("All fields are required");
+      console.log("error");
+    }
+    // $("#submit").on("click", submit);
+  };
+
+  // $("#myModal").on("shown.bs.modal", function() {
+  //   $("#myInput").trigger("focus");
+  // });
+
+  // const employeeSelection = function(selection, submit) {};
+
+  // const employeeEquation = function() {
+  //   const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  //   const sum = function(strUser) {
+  //     let scores = strUser.scores;
+  //     let reducedScores = scores.map(Number);
+  //     let userSum = reducedScores.reduce(reducer);
+  //     return userSum;
+
+  //     // let userSum = sum(strUser);
+  //     let lowestDifference = 100;
+  //     let employeeRank = 100;
+  //     let employeeSelection;
+  //     let employeeSum = 0;
+
+  //     for (let i = 0; i < employeeList.length; i++) {
+  //       employeeSum = employeesList[i].scores.reduce(reducer);
+  //       employeeRank = Math.abs(parseInt(userSum) - parseInt(employeeSum));
+
+  //       if (employeeRank <= lowestDifference) {
+  //         lowestDifference = employeeRank;
+  //         employeeSelection = i;
+  //       }
+  //     }
+  //     const selection = {
+  //       name: employeesList[employeeSelection].name,
+  //       photo: employeesList[employeeSelection].link,
+  //       rank: employeeRank
+  //     };
+  //   };
+  // };
+
+  $("#submit").on("click", submit);
+});
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-const employees = require("../data/employees");
+const employees = require("../data/employees.js");
 
 module.exports = function(app) {
   app.get("/api/employees", function(req, res) {
@@ -7,13 +7,44 @@ module.exports = function(app) {
 
   app.post("/api/employees", function(req, res) {
     const strUser = req.body;
-    const userScores = userData.userSum;
-    employees.post(strUser);
+    console.log("req", req.body);
+    const userScores = strUser.userSum;
+    // employees.post(strUser);
     const employeeSelection = {
       name: "",
       photo: "",
       employeeDifference: Infinity
     };
-    console.log(userData);
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let scores = strUser.scores;
+    let reducedScores = scores.map(Number);
+    let userSum = reducedScores.reduce(reducer);
+    // return userSum;
+
+    // let userSum = sum(strUser);
+    let lowestDifference = 100;
+    let employeeRank = 100;
+    // let employeeSelection;
+    let employeeSum = 0;
+    let employeeIndex;
+
+    for (let i = 0; i < employees.length; i++) {
+      employeeSum = employees[i].scores.reduce(reducer);
+      employeeRank = Math.abs(parseInt(userSum) - parseInt(employeeSum));
+
+      if (employeeRank <= lowestDifference) {
+        lowestDifference = employeeRank;
+        employeeIndex = i;
+      }
+      console.log(employeeIndex);
+    }
+    const selection = {
+      name: employees[employeeIndex].name,
+      photo: employees[employeeIndex].link,
+      rank: employeeRank
+    };
+    console.log(strUser);
+    res.json(selection);
   });
 };
